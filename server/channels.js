20b110f1048150b4aa25cacdf3dfbd7fb331570e
Saul Maddox <saul.maddox@gmail.com>
2022-07-06 10:27:39 -0400

    feat: add comments for apis to be created

--- server/channels.js
+++ server/channels.js
@@ -1,1 +1,7 @@
-module.exports = ({ app, knex }) => {};
+module.exports = ({ app, knex }) => {
+  // load all channels by a user
+  // delete a channel
+  // add a user to a channel
+  // remove a user from a channel
+  // create a channel and pass in an array of users to be part of that channel.
+};
--- server/messages.js
+++ server/messages.js
@@ -1,1 +1,7 @@
-module.exports = ({ app, knex }) => {};
+module.exports = ({ app, knex }) => {
+  // get messages for a channel. pass in the channel id. limit to 50 messages at a time.
+  // create a message for a channel
+  // delete a message for a channel
+  // update a message for a channel
+  // TODO: eventually... recent messages that are unread by the user
+};
